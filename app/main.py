d26bfc2afc5855d7246d86dd127149eb0c2baa85
stiny-namake <email.escolar174@gmail.com>
2025-08-21 20:53:28 -0300

    pain

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from app.routers import health
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
+
+app.include_router(health.router)
